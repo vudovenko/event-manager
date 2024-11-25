ccacfeb90dc195a6891a2c8bd35be52221232b01
vudovenko <vladislav.udovenko0@yandex.ru>
2024-11-25 13:41:09 +0500

    git init

---
+++ src/main/java/dev/vudovenko/eventmanagement/EventManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.vudovenko.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vudovenko/eventmanagement/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vudovenko.eventmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
