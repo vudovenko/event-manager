c638d2c1834546522a1deb9973af05f2d27567a4
vudovenko <vladislav.udovenko0@yandex.ru>
2024-11-25 15:53:41 +0500

    добавлен DTO для ошибок и общий обработчик исключений

---
+++ src/main/java/dev/vudovenko/eventmanagement/exceptionHandling/handler/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package dev.vudovenko.eventmanagement.exceptionHandling.handler;
+
+import dev.vudovenko.eventmanagement.exceptionHandling.dto.ErrorMessageResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@Log4j2
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessageResponse> handleGenericException(
+            Exception e
+    ) {
+        log.error("Server error", e);
+        ErrorMessageResponse errorDto = ErrorMessageResponse.of(
+                "Server error",
+                e.getMessage()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorDto);
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorMessageResponse> handleValidationException(
+            MethodArgumentNotValidException e
+    ) {
+        log.error("Got validation exception", e);
+
+        String detailedMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorMessageResponse errorDto = ErrorMessageResponse.of(
+                "Request validation failed",
+                detailedMessage
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorDto);
+    }
+}
