8df20656a2127ba12a41ac72c67f031517c2decf
vudovenko <vladislav.udovenko0@yandex.ru>
2024-12-03 00:03:11 +0500

    добавлено создание дефолтных пользователей при старте приложения

---
+++ src/main/java/dev/vudovenko/eventmanagement/users/services/DefaultUserInitializer.java
@@ -0,0 +1,59 @@
+package dev.vudovenko.eventmanagement.users.services;
+
+import dev.vudovenko.eventmanagement.security.jwt.JwtTokenManager;
+import dev.vudovenko.eventmanagement.users.entity.UserEntity;
+import dev.vudovenko.eventmanagement.users.repositories.UserRepository;
+import dev.vudovenko.eventmanagement.users.userRoles.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DefaultUserInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenManager jwtTokenManager;
+
+    public static final String DEFAULT_ADMIN_LOGIN = "admin";
+    public static final String DEFAULT_USER_LOGIN = "user";
+
+    private static volatile boolean isUsersInitialized = false;
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void createDefaultUsers() {
+        synchronized (DefaultUserInitializer.class) {
+            if (!isUsersInitialized) {
+                createUser(DEFAULT_ADMIN_LOGIN, "admin", 20, UserRole.ADMIN);
+                createUser(DEFAULT_USER_LOGIN, "user", 40, UserRole.USER);
+
+                isUsersInitialized = true;
+            }
+        }
+    }
+
+    private void createUser(
+            String login,
+            String password,
+            Integer age,
+            UserRole role
+    ) {
+        if (userRepository.existsByLogin(login)) {
+            return;
+        }
+
+        String hashedPass = passwordEncoder.encode(password);
+        UserEntity userToSave = new UserEntity(
+                null,
+                login,
+                hashedPass,
+                age,
+                role
+        );
+
+        userRepository.save(userToSave);
+    }
+}
