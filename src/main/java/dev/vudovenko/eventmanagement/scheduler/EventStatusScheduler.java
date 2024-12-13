6c568425f0e75e71a4ac40952591711fbacc4b43
vudovenko <vladislav.udovenko0@yandex.ru>
2024-12-14 02:44:53 +0500

    вынес параметр частоты обновления статусов мероприятий в application.yaml

--- src/main/java/dev/vudovenko/eventmanagement/scheduler/EventStatusScheduler.java
+++ src/main/java/dev/vudovenko/eventmanagement/scheduler/EventStatusScheduler.java
@@ -2,17 +2,21 @@ package dev.vudovenko.eventmanagement.scheduler;
 
 import dev.vudovenko.eventmanagement.events.services.EventService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class EventStatusScheduler {
 
     private final EventService eventService;
 
-    @Scheduled(fixedRateString = "PT01M")
+    @Scheduled(fixedRateString = "${scheduler.event.statuses.ISO}")
     public void updateEventStatuses() {
+        log.info("Updating event statuses");
+
         eventService.updateEventStatuses();
     }
 }
